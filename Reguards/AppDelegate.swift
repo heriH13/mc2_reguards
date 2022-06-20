dbc9fcae85d2da8b33430f843d2080a8c9f354d2
heriH13 <heri.hermawan13@gmail.com>
2022-06-20 15:56:36 +0700

    Added PKG, modified Appdelegate, plist for sendbirds

--- Reguards/AppDelegate.swift
+++ Reguards/AppDelegate.swift
@@ -7,14 +7,24 @@
 
 import UIKit
 import CoreData
+import Commons
+import UserNotifications
+import SendBirdCalls
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+//    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let appId: String = "80F3FCA1-7C5B-40D3-8AE5-1F075AA3526E"
+                    SendBirdCall.configure(appId: appId)
+                    SendBirdCall.addDelegate(self, identifier: "AppDelegate")
+                    
+        remoteNotificationsRegistration(application)
+        
         return true
     }
 
@@ -76,6 +86,59 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+            SendBirdCall.application(application, didReceiveRemoteNotification: userInfo)
+        }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+            UserDefaults.standard.remotePushToken = deviceToken
+            SendBirdCall.registerRemotePush(token: deviceToken, completionHandler: nil)
+        }
+    
+    func remoteNotificationsRegistration(_ application: UIApplication) {
+            application.registerForRemoteNotifications()
+            
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                guard error == nil else {
+                    print("Error while requesting permission for notifications.")
+                    return
+                }
+                
+                // If success is true, the permission is given and notifications will be delivered.
+            }
+        }
 
 }
 
+extension AppDelegate: SendBirdCallDelegate {
+    func didStartRinging(_ call: DirectCall) {
+        DispatchQueue.main.async {
+            let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+            guard let callerId = call.caller?.userId,
+                  let viewController = storyboard.instantiateViewController(withIdentifier: "CallingViewController") as? CallingViewController else { return }
+            
+            let alertController = UIAlertController(title: "Incoming Call",
+                                                    message: "Incoming \(call.isVideoCall ? "Video" : "Audio") Call from \(callerId)",
+                                                    preferredStyle: .alert)
+            
+            let acceptAction = UIAlertAction(title: "Accept", style: .default, handler: { (_) in
+                call.accept(with: AcceptParams(callOptions: CallOptions(isAudioEnabled: true, isVideoEnabled: true)))
+                viewController.call = call
+                DispatchQueue.main.async {
+                    alertController.dismiss(animated: true, completion: nil)
+                    UIViewController.topViewController?.present(viewController, animated: true, completion: nil)
+                }
+            })
+            let declineAction = UIAlertAction(title: "Decline", style: .destructive, handler: { (_) in
+                call.end()
+            })
+            
+            alertController.addAction(acceptAction)
+            alertController.addAction(declineAction)
+            
+            UIViewController.topViewController?.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
