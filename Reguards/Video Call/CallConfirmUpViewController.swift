3f75bedd9d305239aecd9d72a2087ca277744c54
Fabriano28 <ferumbromin@gmail.com>
2022-06-28 17:58:57 +0700

    Revised video call countdown to countupp

---
+++ Reguards/Video Call/CallConfirmUpViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CallConfirmUpViewController.swift
+//  Reguards
+//
+//  Created by Farrel Brian on 28/06/22.
+//
+
+import UIKit
+import SendBirdCalls
+import Commons
+import Photos
+
+class CallConfirmUpViewController: UIViewController {
+
+
+    @IBOutlet weak var requestBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authenticate()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        startPulseAnimation()
+    }
+    
+    @IBAction func onTapBtn(_ sender: UIButton) {
+        let calleeId = "TestUser1"
+        
+        let dialParams = DialParams(calleeId: calleeId,
+                                    isVideoCall: true,
+                                    callOptions: CallOptions(isAudioEnabled: true, isVideoEnabled: true),
+                                    customItems: [:])
+        SendBirdCall.dial(with: dialParams) { (call, error) in
+            guard let call = call, error == nil else { return }
+            
+//            DispatchQueue.main.async {
+        self.performSegue(withIdentifier: "dial", sender: call)
+//            }
+        }
+    }
+    
+//    @IBAction func unwindToCallConfirm(_ unwindSegue: UIStoryboardSegue) {
+//
+//    }
+    
+    func startPulseAnimation() {
+        
+        requestBtn?.layer.cornerRadius = (requestBtn?.frame.height ?? 200) / 2
+        requestBtn?.clipsToBounds = true
+        
+        let pulse1 = PulseAnimation(numOfPulse: Float.infinity, radius: 160, position: requestBtn.center)
+        pulse1.animationDuration = 2
+        pulse1.backgroundColor = UIColor(red: 0.10, green: 0.10, blue: 0.20, alpha: 1.00).cgColor
+        self.view.layer.insertSublayer(pulse1, below: self.view.layer)
+        
+        let pulse = PulseAnimation(numOfPulse: Float.infinity, radius: 130, position: requestBtn.center)
+        pulse.animationDuration = 2
+        pulse.backgroundColor = UIColor(red: 0.18, green: 0.18, blue: 0.30, alpha: 1.00).cgColor
+        self.view.layer.insertSublayer(pulse, below: self.view.layer)
+        
+        self.view.bringSubviewToFront(requestBtn)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? CallingViewController,
+              let call = sender as? DirectCall else { return }
+
+        destination.call = call
+    }
+
+    func authenticate() {
+        let userId = "085156595835"
+        
+        let authenticateParams = AuthenticateParams(userId: userId)
+        SendBirdCall.authenticate(with: authenticateParams) { (user, error) in
+            guard let user = user, error == nil else { return }
+            
+            SendBirdCall.registerRemotePush(token: UserDefaults.standard.remotePushToken, completionHandler: nil)
+            
+            print("Successfully authenticated with userId: \(user.userId)")
+        }
+    }
+    
+    func deauthenticate() {
+        SendBirdCall.deauthenticate { (error) in
+            guard error == nil else { return }
+            
+            print("Successfully deauthenticated from SendBirdCalls")
+        }
+    }
+
+}
--- Reguards/Video Call/CallingViewController.swift
+++ Reguards/Video Call/CallingViewController.swift
@@ -13,20 +13,35 @@ import Photos
 
 class CallingViewController: UIViewController {
 
+    var countVC: CountupViewController?
+    
     var call: DirectCall!
     
     @IBOutlet weak var videoView: UIView!
     @IBOutlet weak var endBtn: UIButton!
+    @IBOutlet weak var countUpView: UIView!
+    @IBOutlet weak var cancelBtn: UIButton!
+    @IBOutlet weak var requestGuardianBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        endBtn?.layer.cornerRadius = (endBtn?.frame.height ?? 68) / 2
+        cancelBtn.layer.borderWidth = 2
+        cancelBtn.layer.borderColor = UIColor(red: 0.22, green: 0.22, blue: 0.37, alpha: 1.00).cgColor
+        cancelBtn.layer.cornerRadius = 25
+        
+        endBtn.layer.borderWidth = 2
+        endBtn.layer.borderColor = UIColor(red: 0.22, green: 0.22, blue: 0.37, alpha: 1.00).cgColor
+        endBtn.layer.cornerRadius = 25
+        
+        endBtn.isHidden = true
+        requestGuardianBtn.isHidden = true
+        cancelBtn.isHidden = false
         
         if call != nil {
-            let localView = SendBirdVideoView(frame: videoView.frame)
-            videoView.embed(localView)
-            call?.updateLocalVideoView(localView)
+            let localView2 = SendBirdVideoView(frame: self.videoView.frame)
+            videoView.embed(localView2)
+            call?.updateLocalVideoView(localView2)
             
             let remoteView = SendBirdVideoView(frame: self.view.frame)
             view.embed(remoteView)
@@ -39,8 +54,20 @@ class CallingViewController: UIViewController {
     @IBAction
     func didTapEndCallButton(_ sender: UIButton) {
         call?.end()
-        deauthenticate()
-//        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func didTapCancelBtn(_ sender: Any) {
+        call?.end()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? CountupViewController {
+                    // save reference to VC embedded in Container View
+                    self.countVC = vc
+                }
+    }
+    
+    @IBAction func didTapRequestGuardianBtn(_ sender: Any) {
     }
     
     func deauthenticate() {
@@ -54,6 +81,11 @@ class CallingViewController: UIViewController {
 
 extension CallingViewController: DirectCallDelegate {
     func didConnect(_ call: DirectCall) {
+        countVC?.t?.invalidate()
+        countUpView.isHidden = true
+        cancelBtn.isHidden = true
+        endBtn.isHidden = false
+        requestGuardianBtn.isHidden = false
         print("Call connected")
     }
     
